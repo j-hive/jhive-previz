187d5d30d183eb936fedd87274116ae91d2a0392
Jennifer Scora <jscora@sidratresearch.com>
2024-10-22 14:02:05 -0400

    added tests for conversions, cleaned up test_dataproc

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -1,11 +1,30 @@
 import pytest
+import pandas as pd
+import numpy as np
 
 from jhive_previz import conversions as conv
+from jhive_previz import dataproc
 
 
 def test_get_conversion_function():
-    pass
+    """Make sure that get_conversion_function works as expected"""
+
+    test = conv.get_conversion_function("microJansky", "magnitude")
+
+    assert test == conv.flux_to_mag
 
 
-def test_flux_to_mag():
-    pass
+def test_flux_to_mag(load_config):
+    """Make sure that flux_to_mag works as expected"""
+
+    file_path = dataproc.get_data_output_filepath(load_config[0])
+    df = pd.read_csv(file_path)
+
+    converted = conv.flux_to_mag(df["abmag_f444w"], load_config[1]["abmag_f444w"])
+
+    test_result = (
+        -2.5 * np.log10(df["abmag_f444w"].iloc[6])
+        + load_config[1]["abmag_f444w"]["zero_point"]
+    )
+    assert converted.iloc[6] == test_result
+    assert isinstance(converted, pd.Series)
